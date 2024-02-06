46189fc50c1aad850ca43fc3a389a4c433c0ce0a
Hairum Qureshi <hqureshi@udel.edu>
2024-02-05 22:30:54 -0500

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
+                Hairum Qureshi <br />
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
