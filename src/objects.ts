8a976fc534c151fcda1d4094a29c575b21560a90
Hairum Qureshi <hqureshi@udel.edu>
2024-02-21 12:58:22 -0500

    completed objects task and removed unnecessary commented code in arrays.ts

--- src/objects.ts
+++ src/objects.ts
@@ -10,7 +10,16 @@ export function makeBlankQuestion(
     name: string,
     type: QuestionType
 ): Question {
-    return {};
+    return {
+        id,
+        name,
+        body: "",
+        type,
+        options: [],
+        expected: "",
+        points: 1,
+        published: false
+    };
 }
 
 /**
@@ -21,7 +30,7 @@ export function makeBlankQuestion(
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
 export function isCorrect(question: Question, answer: string): boolean {
-    return false;
+    return answer.toLowerCase().trim() == question.expected.toLowerCase();
 }
 
 /**
@@ -31,7 +40,12 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    if (question.type == "short_answer_question") return true;
+    else {
+        return question.options.some(
+            (option: string): boolean => option == answer
+        );
+    }
 }
 
 /**
@@ -41,7 +55,8 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    const trimmedQName: string = question.name.slice(0, 10);
+    return `${question.id}: ${trimmedQName}`;
 }
 
 /**
@@ -62,7 +77,15 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    const { name, body, type, options } = question;
+    let formattedOptions = "# " + name + "\n" + body;
+    // let formattedOptions = `
+    //     # ${name}
+    //     ${body} \n`;
+    if (type === "multiple_choice_question") {
+        options.map((option) => (formattedOptions += "\n- " + option));
+    }
+    return formattedOptions;
 }
 
 /**
@@ -70,7 +93,7 @@ export function toMarkdown(question: Question): string {
  * `newName`.
  */
 export function renameQuestion(question: Question, newName: string): Question {
-    return question;
+    return { ...question, name: newName };
 }
 
 /**
@@ -79,7 +102,10 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    return {
+        ...question,
+        published: !question.published
+    };
 }
 
 /**
@@ -89,7 +115,14 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return oldQuestion;
+    const newQuestion: Question = {
+        ...oldQuestion,
+        id,
+        name: `Copy of ${oldQuestion.name}`,
+        published: false
+    };
+
+    return newQuestion;
 }
 
 /**
@@ -100,7 +133,11 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    const newVersion: Question = {
+        ...question,
+        options: [...question.options, newOption]
+    };
+    return newVersion;
 }
 
 /**
@@ -117,5 +154,16 @@ export function mergeQuestion(
     contentQuestion: Question,
     { points }: { points: number }
 ): Question {
-    return contentQuestion;
+    const { body, type, options, expected } = contentQuestion;
+    const newQuestion: Question = {
+        id,
+        name,
+        body,
+        type,
+        options,
+        expected,
+        points,
+        published: false
+    };
+    return newQuestion;
 }
